5910e3ddc8fd57da29fafff980075f6c2574d0dd
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-17 23:56:42 +0100

    Add navbar and header components to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Header from "@/components/Header/Header";
+import NavBar from "@/components/NavBar/NavBar";
+
 export default function Home() {
-    return <div>Hello, World!</div>;
+    return (
+        <main>
+            <NavBar />
+            <Header />
+        </main>
+    );
 }
