1c4720691959b28b37d6fe45f4a276d82e10db9c
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-17 22:41:49 +0100

    Add Poppins font

---
+++ src/app/styles/fonts.ts
@@ -0,0 +1,17 @@
+import { Poppins } from "next/font/google";
+
+const Poppins200 = Poppins({ weight: "200", subsets: ["latin"] });
+const Poppins300 = Poppins({ weight: "300", subsets: ["latin"] });
+const Poppins400 = Poppins({ weight: "400", subsets: ["latin"] });
+const Poppins500 = Poppins({ weight: "500", subsets: ["latin"] });
+const Poppins600 = Poppins({ weight: "600", subsets: ["latin"] });
+const Poppins700 = Poppins({ weight: "700", subsets: ["latin"] });
+
+export {
+    Poppins200,
+    Poppins300,
+    Poppins400,
+    Poppins500,
+    Poppins600,
+    Poppins700,
+};
