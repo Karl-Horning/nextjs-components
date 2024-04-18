18adc7a0561fc932fba4206358e9f19cb8bce8f4
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-18 21:45:21 +0100

    Add responsive navbar and CSS module

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { FaArrowUpRightFromSquare, FaBars } from "react-icons/fa6";
 import { Poppins500 } from "@/fonts";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import style from "./NavBar.module.css";
 
 export default function NavBar() {
     const [scrollY, setScrollY] = useState(0);
@@ -25,6 +26,12 @@ export default function NavBar() {
         };
     }, []);
 
+    const toggleMenu = () => {
+        const menuLinks = document.getElementById("menu-links");
+        menuLinks?.classList.toggle("max-h-full");
+        menuLinks?.classList.toggle("max-h-0");
+    };
+
     return (
         <nav
             id="navbar"
@@ -33,31 +40,39 @@ export default function NavBar() {
         >
             <Image
                 src="/example-images/logo.png"
-                className="cursor-pointer"
+                className="w-[150px] cursor-pointer md:w-[220px]"
                 alt="logo"
                 height={100}
                 width={220}
             />
 
-            <ul>
+            <ul
+                id="menu-links"
+                className={`${style["menu-links"]} absolute left-0 top-full max-h-0 w-full overflow-hidden bg-black text-center text-white md:static md:top-auto md:max-h-full md:w-auto md:bg-transparent md:text-left md:text-black md:text-opacity-100`}
+            >
                 <li className="mx-5 my-3 inline-block cursor-pointer font-medium">
                     Home
                 </li>
-                <li className="mx-5 my-3 inline-block cursor-pointer font-medium">
+                <li className="m-[10px] inline-block cursor-pointer font-medium md:mx-5 md:my-3">
                     Services
                 </li>
-                <li className="mx-5 my-3 inline-block cursor-pointer font-medium">
+                <li className="m-[10px] inline-block cursor-pointer font-medium md:mx-5 md:my-3">
                     Projects
                 </li>
-                <li className="mx-5 my-3 inline-block cursor-pointer font-medium">
+                <li className="m-[10px] inline-block cursor-pointer font-medium md:mx-5 md:my-3">
                     About Us
                 </li>
             </ul>
 
-            <button className="inline-flex cursor-pointer items-center rounded-[40px] bg-black px-6 py-4 text-white">
+            <button className="hidden cursor-pointer items-center rounded-[40px] bg-black px-6 py-4 text-white md:inline-flex">
                 <span>Get Started</span>
                 <FaArrowUpRightFromSquare className="ml-2" />
             </button>
+
+            <FaBars
+                className="block h-8 w-8 cursor-pointer md:hidden"
+                onClick={toggleMenu}
+            />
         </nav>
     );
 }
